d7a8677cbc5ee6cdb7263d3a79871f7312674a6c
Larry Garfield <larry@garfieldtech.com>
2019-12-19 17:33:18 -0600

    Map point class to point struct.

--- points-test.php
+++ points-test.php
@@ -7,8 +7,16 @@ $p2 = new Point(7, 9);
 //$ffi = FFI::load(__DIR__ . "/points.h");
 //$ffi = FFI::scope("POINTS");
 
-$ffi = FFI::cdef(file_get_contents('points.h'), 'points.o');
+$ffi = FFI::cdef(file_get_contents('points.h'), __DIR__ . '/points.so');
 
-$d = $ffi->distance($p1, $p2);
+$cp1 = $ffi->new('struct point');
+$cp2 = $ffi->new('struct point');
+
+$cp1->x = $p1->x;
+$cp1->y = $p1->y;
+$cp2->x = $p2->x;
+$cp2->y = $p2->y;
+
+$d = $ffi->distance($cp1, $cp2);
 
 print "Distance is $d\n";
