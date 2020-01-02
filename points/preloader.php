76b0ad4f80781de3340c2b09b0b8ab0c098bf402
Larry Garfield <larry@garfieldtech.com>
2020-01-02 14:44:56 -0600

    Add debug text.

--- points/preloader.php
+++ points/preloader.php
@@ -1,5 +1,9 @@
 <?php
 declare(strict_types=1);
 
+print "START PRELOADER" . PHP_EOL;
+
 FFI::load(__DIR__ . "/points.h");
 opcache_compile_file(__DIR__ . "/classes.php");
+
+print "END PRELOADER" . PHP_EOL;
