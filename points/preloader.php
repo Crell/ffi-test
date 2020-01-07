19066184ab91943dcca70f365997f39df2d3d6a4
Larry Garfield <larry@garfieldtech.com>
2020-01-07 15:49:23 -0600

    Remove debug code.

--- points/preloader.php
+++ points/preloader.php
@@ -1,9 +1,5 @@
 <?php
 declare(strict_types=1);
 
-print "START PRELOADER" . PHP_EOL;
-
 FFI::load(__DIR__ . "/points.h");
 opcache_compile_file(__DIR__ . "/classes.php");
-
-print "END PRELOADER" . PHP_EOL;
