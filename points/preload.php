e1f17aeb7bfab85facef6f5550f5856a64257c66
Larry Garfield <larry@garfieldtech.com>
2020-01-07 16:02:17 -0600

    Refactor preload version to be nicer.

--- points/preload.php
+++ points/preload.php
@@ -1,20 +1,11 @@
 <?php
 declare(strict_types=1);
 
-// Get a handle on the appropriate scope.
-$ffi = FFI::scope('POINTS');
-
 $p1 = new Point(3, 4);
 $p2 = new Point(7, 9);
 
-$cp1 = $ffi->new('struct point');
-$cp2 = $ffi->new('struct point');
+$api = new PointApi();
 
-$cp1->x = $p1->x;
-$cp1->y = $p1->y;
-$cp2->x = $p2->x;
-$cp2->y = $p2->y;
+$d2 = $api->distance($p1, $p2);
 
-$d = $ffi->distance($cp1, $cp2);
-
-print "Distance is $d\n";
+print "Distance is $d2\n";
