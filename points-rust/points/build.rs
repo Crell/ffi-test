7fbd0038f7a6e29fdc50f29ed9c430ec1817535a
Larry Garfield <larry@garfieldtech.com>
2020-01-10 14:29:08 -0600

    Generate the header file with the PHP defines in it automatically.

--- points-rust/points/build.rs
+++ points-rust/points/build.rs
@@ -2,11 +2,20 @@
 extern crate cbindgen;
 
 use std::env;
+use std::io::prelude::*;
+use std::fs::File;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let mut file = File::create("points.h")?;
+
+    writeln!(file, "#define FFI_SCOPE \"POINTS\"")?;
+    writeln!(file, "#define FFI_LIB \"./target/release/libpoints.so\"")?;
 
     cbindgen::generate(&crate_dir)
         .unwrap()
-        .write_to_file("points.h");
+        .write(file);
+
+    Ok(())
 }
