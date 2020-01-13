aa87255ddfd7598d84d7bf9e3ec1517625ff3b27
Larry Garfield <larry@garfieldtech.com>
2020-01-13 17:14:22 -0600

    Add tests to Rust version.

--- points-rust/points/src/lib.rs
+++ points-rust/points/src/lib.rs
@@ -5,7 +5,7 @@ pub struct Point {
     pub y: u8,
 }
 
-pub fn distance(p1: Point, p2: Point) -> f64 {
+fn compute_distance(p1: Point, p2: Point) -> f64 {
     let a_squared = (p2.x as f64 - p1.x as f64).powf(2.0);
     let b_squared = (p2.y as f64 - p1.y as f64).powf(2.0);
 
@@ -14,6 +14,21 @@ pub fn distance(p1: Point, p2: Point) -> f64 {
 
 
 #[no_mangle]
-pub unsafe extern "C" fn compute_distance(p1: Point, p2: Point) -> f64 {
-    distance(p1, p2)
+pub unsafe extern "C" fn distance(p1: Point, p2: Point) -> f64 {
+    compute_distance(p1, p2)
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_distance() {
+        let p1 = Point{x: 3, y: 2};
+        let p2 = Point{x: 6, y: 6};
+        assert_eq!(compute_distance(p1, p2), 5.0);
+    }
+
 }
