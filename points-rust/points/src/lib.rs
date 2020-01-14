139388e7f6989b33ebdf6c82bea7d182fc7ea91d
Larry Garfield <larry@garfieldtech.com>
2020-01-14 16:42:36 -0600

    The magic incantation that lets Rust build both an executable and a clib at the same time.

--- points-rust/points/src/lib.rs
+++ points-rust/points/src/lib.rs
@@ -5,7 +5,7 @@ pub struct Point {
     pub y: u8,
 }
 
-fn compute_distance(p1: Point, p2: Point) -> f64 {
+pub fn compute_distance(p1: Point, p2: Point) -> f64 {
     let a_squared = (p2.x as f64 - p1.x as f64).powf(2.0);
     let b_squared = (p2.y as f64 - p1.y as f64).powf(2.0);
 
