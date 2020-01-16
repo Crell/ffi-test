9447e74da3f3aec68cc2c3c4547663fc109a3ed0
Larry Garfield <larry@garfieldtech.com>
2020-01-16 09:07:50 -0600

    Force build

--- points-rust/points/src/lib.rs
+++ points-rust/points/src/lib.rs
@@ -14,7 +14,7 @@ pub fn compute_distance(p1: Point, p2: Point) -> f64 {
 
 #[no_mangle]
 pub unsafe extern "C" fn distance(p1: Point, p2: Point) -> f64 {
-    compute_distance(p1, p2)
+    return compute_distance(p1, p2);
 }
 
 #[cfg(test)]
